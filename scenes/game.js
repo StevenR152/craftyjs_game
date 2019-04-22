a336670205e4410101537d12f01d13874b68e5e9
Steven <steve.rutherford12@gmail.com>
2019-04-22 12:35:56 +0100

    extract world class

--- scenes/game.js
+++ scenes/game.js
@@ -180,27 +180,8 @@ var decorationIdToImage = {
 	}
 };
 
-function renderLayer (layerIndex, layerName, idToEntityDef) {
-	for(var rowIndex = 0; rowIndex < world[layerIndex][layerName].length; rowIndex++) {
-		for(var colIndex = 0; colIndex < world[layerIndex][layerName][rowIndex].length; colIndex++) {
-			var tileId = world[layerIndex][layerName][rowIndex][colIndex];
-			var entityDef = idToEntityDef[tileId];
-			if(typeof entityDef !== 'undefined') {
-				var tileType = entityDef.image;
-				if(tileId == 0 && layerIndex != world.length - 1) {
-					tileType = ""; // Water should only be on base layer.
-				}
-				 Crafty.e('2D, DOM, ' + tileType)
-					.attr({x: colIndex * 101, y: rowIndex * 81 + layerIndex * 40 - 121, w: 101, h: 171})	
-			}
-		}
-	}
-}
 Crafty.defineScene("Game", function() {
+	var world = new World(world);
+	world.render();
 	Crafty.viewport.scale(0.5);
-	for(var layerIndex = world.length -1; layerIndex >= 0; layerIndex--){
-		renderLayer(layerIndex, "base", tileIdToImage);
-		renderLayer(layerIndex, "object", objectIdToImage)
-		renderLayer(layerIndex, "decorative", decorationIdToImage)
-	}
 }); 
---
+++ world/World.js
@@ -0,0 +1,31 @@
+class World {
+
+	constructor(world) {
+		this._world = world;
+	} 
+
+	renderLayer (layerIndex, layerName, idToEntityDef) {
+		for(var rowIndex = 0; rowIndex < world[layerIndex][layerName].length; rowIndex++) {
+			for(var colIndex = 0; colIndex < world[layerIndex][layerName][rowIndex].length; colIndex++) {
+				var tileId = world[layerIndex][layerName][rowIndex][colIndex];
+				var entityDef = idToEntityDef[tileId];
+				if(typeof entityDef !== 'undefined') {
+					var tileType = entityDef.image;
+					if(tileId == 0 && layerIndex != world.length - 1) {
+						tileType = ""; // Water should only be on base layer.
+					}
+					 Crafty.e('2D, DOM, ' + tileType)
+						.attr({x: colIndex * 101, y: rowIndex * 81 + layerIndex * 40 - 121, w: 101, h: 171})	
+				}
+			}
+		}
+	}
+
+	render() {
+		for(var layerIndex = world.length -1; layerIndex >= 0; layerIndex--){
+			this.renderLayer(layerIndex, "base", tileIdToImage);
+			this.renderLayer(layerIndex, "object", objectIdToImage)
+			this.renderLayer(layerIndex, "decorative", decorationIdToImage)
+		}
+	}
+}
