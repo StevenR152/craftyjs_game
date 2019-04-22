5a46f9366009c0d28f75ea08c91859be9a55bf52
Steven <steve.rutherford12@gmail.com>
2019-04-22 12:05:09 +0100

    Added cute tile images and loaded a world.

--- scenes/loading.js
+++ scenes/loading.js
@@ -1,4 +1,52 @@
-Crafty.defineScene("Loading", function() {
+var assetsObj = {
+    "audio": {
+    },
+    "images": [],
+    "sprites": {
+        "images/tiles/GrassBlock.png": {
+            "tile": 101,
+            "tileh": 171,
+            "map": { "grassblock": [0,0] }
+        },
+        "images/tiles/Water Block.png": {
+            "tile": 101,
+            "tileh": 171,
+            "map": { "waterblock": [0,0] }
+        },
+        "images/tiles/Dirt Block.png": {
+            "tile": 101,
+            "tileh": 171,
+            "map": { "sandblock": [0,0] }
+        },
+        "images/tiles/Stone Block.png": {
+            "tile": 101,
+            "tileh": 171,
+            "map": { "stoneblock": [0,0] }
+        },
+        "images/tiles/Ramp South.png": {
+            "tile": 101,
+            "tileh": 171,
+            "map": { "rampsouth": [0,0] }
+        },
+        "images/tiles/Ramp East.png": {
+            "tile": 101,
+            "tileh": 171,
+            "map": { "rampeast": [0,0] }
+        },
+        "images/tiles/Ramp West.png": {
+            "tile": 101,
+            "tileh": 171,
+            "map": { "rampwest": [0,0] }
+        },
+        "images/tiles/Ramp North.png": {
+            "tile": 101,
+            "tileh": 171,
+            "map": { "rampnorth": [0,0] }
+        },
+    },
+};
+
+function presentLoadingScreen() {
     Crafty.background("#AAA");
     Crafty.e("2D, DOM, Text")
           .attr({ w: 200, h: 50, x: 300, y: 280 })
@@ -6,17 +54,18 @@ Crafty.defineScene("Loading", function() {
           .textFont({ size: '20px', weight: 'bold' })
           .textAlign("center")
           .textColor("#111");
- 	
-    // Game Assets would be a list of images, but this tutorial doesn't use them.
-    var gameAssets = {};
+}
 
-  	Crafty.load(gameAssets, function(){
-       setTimeout(function () {
+Crafty.defineScene("Loading", function() {
+ 	  presentLoadingScreen();
+
+  	Crafty.load(assetsObj, function(){
+      console.log("Assets loaded, starting game.");
          Crafty.scene('Game');
-       }, 1000);
-    },  function () {
+    },  function (e) {
       console.log("Crafty Load Issue");
     },  function (e) {
       console.log("Crafty Load Error", e);
+
     });
   });
